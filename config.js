1da1adeb1186a39f99577573093a6b290f289bb8
ganeshtedi <baisaniganesh@gmail.com>
2020-04-10 20:03:01 +0530

    uu

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-export const uri = "mongodb+srv://Ganeshatlas:ganeshatlas@cluster0-l2jlg.mongodb.net/test?retryWrites=true&w=majority";
+module.exports={
+    uri :"mongodb+srv://Ganeshatlas:ganeshatlas@cluster0-l2jlg.mongodb.net/test?retryWrites=true&w=majority"
+}
